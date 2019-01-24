0adf9a89c121c56d8ed48807f523e21248afb30f
Bradley McGill <bdm88@live.com>
2019-01-24 13:41:29 -0800

    implemented dynamic server

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+
+mongoose.connect('mongodb://localhost:27017/recipes', {useNewUrlParser: true});
+
+const Recipe = mongoose.model('Recipe', {
+    name: String,
+    info: String
+})
+
+app.get('/recipes', (request, response) =>{
+    Recipe.find().then(data =>{
+        response.json(data)
+    })
+})
 
 module.exports = app;
