0f534e18aac7c4a7dd8ea00303828370d141e6b0
Bradley McGill <bdm88@live.com>
2019-01-25 13:10:43 -0800

    added test for GET recipes

--- test/test-server.js
+++ test/test-server.js
@@ -18,3 +18,20 @@ describe('index page', function(){
             });
     });
 });
+
+describe('recipes', function(){
+    it('should list recipes on GET', function(){
+        return chai.request(app)
+            .get('/recipes')
+            .then(function(res){
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('array');
+                expect(res.body.length).to.be.above(0);
+                res.body.forEach(function(item){
+                    expect(item).to.be.a('object');
+                    expect(item).to.have.all.keys('_id', 'name', 'info');
+                });
+            });
+    });
+});
